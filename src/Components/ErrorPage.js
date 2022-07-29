7426e1e6963f3e9524ec69526a2aa7bfd06e8834
Sujana Bhandari <sujana6589@gmail.com>
2022-07-29 18:51:06 +0200

    Add Error page Image when query is not found

---
+++ src/Components/ErrorPage.js
@@ -0,0 +1,13 @@
+
+const ErrorPage = () => {
+  return (
+    <div className='error-page'>
+      <div className='error-page-container'>
+        {/* <img src={require('./bg-404.jpg')} alt='404' /> */}
+        <p>Sorry</p>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorPage;
